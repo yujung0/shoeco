dc2d0f7d30ee4eafa15fd996bc33c138f4ccb3b3
simseyeon <137152283+simseyeon@users.noreply.github.com>
2023-08-12 04:19:18 +0900

    잠안와서커밋2
    잠안와서커밋2

--- src/main/java/com/shoeco/shoeco/common/paging/ListPagingCreatorDTO.java
+++ src/main/java/com/shoeco/shoeco/common/paging/ListPagingCreatorDTO.java
@@ -26,11 +26,17 @@ public class ListPagingCreatorDTO {
 		this.rowTotal = rowTotal;
 		this.pagingCount = 10;
 		
-		this.endNum = (int)Math.ceil((double)paging.getPageNum() / pagingCount) * pagingCount ;
+		this.endNum = (int)Math.ceil((double)paging.getPageNum() / this.pagingCount) * this.pagingCount ;
 		this.startNum =  this.endNum  - (this.pagingCount - 1) ;
 		this.lastNum = (int)Math.ceil((double)this.rowTotal/paging.getPerPage());
 		this.prev = 1 < this.startNum ;
 		this.next = this.lastNum > this.endNum ;
+		
+		
+		if(this.endNum > this.lastNum) {
+			this.endNum = this.lastNum ;
+		}
+		
 		
 		
 		System.out.println("전달된 페이징 기본데이터-paging: " + this.paging.toString());
