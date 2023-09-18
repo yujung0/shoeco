376dfe153586fa431272bc6ce0ae1fe195ba9489
yujung0 <3qnsdml1@naver.com>
2023-09-18 22:24:16 +0900

    2309182224_장유정

---
+++ src/main/java/com/shoeco/shoeco/admin/mapper/SCAdminMapper.java
@@ -0,0 +1,11 @@
+package com.shoeco.shoeco.admin.mapper;
+
+import com.shoeco.shoeco.admin.model.SCShoesVO;
+
+// 상품 등록 쿼리 실행
+public interface SCAdminMapper {
+
+	// 2309182058 장유정
+	// 상품 등록
+	public void shoesEnroll(SCShoesVO shoes);
+}
