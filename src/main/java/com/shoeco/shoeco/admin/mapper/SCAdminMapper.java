9ca156b96d6d96f789ddfe3ac69d35318a8685f0
yujung0 <3qnsdml1@naver.com>
2023-09-28 20:10:05 +0900

    2309282008_장유정_상품등록 카테고리 구현 시작
    JSON 타입으로 변환하는 방식, 카테고리 리스트 객체를 DB로 호출함

--- src/main/java/com/shoeco/shoeco/admin/mapper/SCAdminMapper.java
+++ src/main/java/com/shoeco/shoeco/admin/mapper/SCAdminMapper.java
@@ -1,5 +1,8 @@
 package com.shoeco.shoeco.admin.mapper;
 
+import java.util.List;
+
+import com.shoeco.shoeco.admin.model.SCCateVO;
 import com.shoeco.shoeco.admin.model.SCShoesVO;
 
 // 상품 등록 쿼리 실행
@@ -8,4 +11,8 @@ public interface SCAdminMapper {
 	// 2309182058 장유정
 	// 상품 등록
 	public void shoesEnroll(SCShoesVO shoes);
+	
+	// 2309281604 장유정
+	// 카테고리 리스트
+	public List<SCCateVO> cateList();
 }
